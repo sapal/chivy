881bf4c3ceae794b2ecdb6a9cb53fe871a79387b
Michał Sapalski <sapalskimichal@gmail.com>
2010-09-30 11:19:07 +0200

    English screenshots, improved translation.

--- chivySources/translation.py
+++ chivySources/translation.py
@@ -6,17 +6,17 @@ from config import Config as config
 
 APP_NAME = "base"
 langs = [config.locale]
-lc, encoding = locale.getdefaultlocale()
+"""lc, encoding = locale.getdefaultlocale()
 if (lc):
     langs += [lc]
 language = os.environ.get('LANGUAGE', None)
 if (language):
     langs += language.split(":")
-langs += ["en_GB","pl_PL"]
+langs += ["pl_PL"]"""
 
 Gettext.bindtextdomain(APP_NAME, config.translationsDir)
 Gettext.textdomain(APP_NAME)
 
-lang = Gettext.translation(APP_NAME, config.translationsDir, languages=langs, fallback = True)
+lang = Gettext.translation(APP_NAME, config.translationsDir, languages=langs, fallback=True)
 def gettext(*args, **kwargs):
     return unicode(lang.gettext(*args, **kwargs),"utf-8")
